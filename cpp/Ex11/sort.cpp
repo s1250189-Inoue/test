9e204317a854d74a3b39ce46a33aaa0cab8c659a
s1250189-Inoue <s1250189aizu@gmail.com>
2019-11-18 14:33:20 +0900

    update

---
+++ cpp/Ex11/sort.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+
+template<Ran>
+void sort(Ran first,Ran last){
+\\...
+}
+
+int main(){
+    
+}
---
+++ cpp/Ex12/Curry.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<functional>
+
+using namespace std;
+
+double deriv_fd(std::function<double(double)> f, double x) {
+  double epsilon = 0.00000095367431640625;
+  return (f(x+epsilon) - f(x-epsilon))/(2.0*epsilon);
+}
+
+auto deriv(std::function<double(double)> f){
+  return [f](double x){
+    return deriv_fd(f,x);
+  };
+}
+
+int main(void) {
+  auto df = deriv([](double x) { return x; });
+  cout << df(1.0) << endl; // should be 1
+  cout << df(2.0) << endl; // should be 1
+}
