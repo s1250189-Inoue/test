f7e14c6c5bff0253723160c5f7b3421073b27b2f
s1250189-Inoue <s1250189aizu@gmail.com>
2019-11-19 13:13:34 +0900

    update

--- cpp/Ex11/sort.cpp
+++ cpp/Ex11/sort.cpp
@@ -1,11 +1,13 @@
 #include<iostream>
+#include<vector>
 
+template<class Ran>
+/*void sort(Ran first,Ran last){
 
-template<Ran>
-void sort(Ran first,Ran last){
-\\...
-}
+}*/
 
 int main(){
-    
+    std::vector<int> v = {1, 2, 3, 6, 5, 4};
+    sort(v.begin(),v.end());
+    return 0;
 }
