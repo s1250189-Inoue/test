6fd3469bd8076931a1392e836447848044f6e503
s1250189-Inoue <s1250189aizu@gmail.com>
2019-11-21 09:08:39 +0900

    update

--- cpp/Ex11/sort.cpp
+++ cpp/Ex11/sort.cpp
@@ -2,12 +2,22 @@
 #include<vector>
 
 template<class Ran>
-/*void sort(Ran first,Ran last){
-
-}*/
+void sort(Ran begin,Ran end){
+    int size = end - begin;
+    for(int i = 0; i < size - 1; i++){
+        for(int j =1 ; j < size - i; j++){
+            if(begin[j-1] > begin[j]){
+                std::swap(begin[j-1],begin[j]);
+            }
+        }
+    }
+}
 
 int main(){
-    std::vector<int> v = {1, 2, 3, 6, 5, 4};
+    std::vector<int> v = {4, 3, 6, 1, 5, 2};
     sort(v.begin(),v.end());
+    for(auto itr = v.begin(); itr != v.end(); ++itr) {
+        std::cout << *itr << "\n";
+    }
     return 0;
 }
